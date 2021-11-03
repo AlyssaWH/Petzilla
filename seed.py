9ad8f73b1dee8e0dbfa3c84afb4ede160d3eae83
Alyssa <hessealy@gmail.com>
2021-11-03 15:03:59 -0700

    started seed data

--- seed.py
+++ seed.py
@@ -8,13 +8,13 @@ os.system('createdb pets')
 model.connect_to_db(server.app)
 model.db.create_all()
 
-user_data = [
-{
+user1 = crud.create_user('bart', 'simpson', '1111111111', '1@test.com',
+'fake', 'phone', 7)
 
-}
-#nested dictionary for users' pets
-#or, hard code data for each user, pet, object etc.
+pet1 = crud.create_pet(1, 'santas little helper', 'dog', 1988, 30)
+pet2 = crud.create_pet(1, 'Snowball', 'cat', 1988, 9)
 
-]
 
-pet_data
+vet1= crud.create_vet(1, 'springfield vet hospital', '1111111111', 'Vetty', "McVetterson")
+
+med1=crud.create_medicine(1, 'glucosamine', 500, 14, '2222', 1)
